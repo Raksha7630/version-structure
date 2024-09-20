3b5b3237fb5947450c2d0f311e1868dc8ac37c38
Raksha7630 <stharakshya63@gmail.com>
2024-09-20 09:16:16 +0300

    Integer number changed
    Number changed to 9 to 5.

--- src/App.java
+++ src/App.java
@@ -1,5 +1,224 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        //number1 is greater than number2
+
+
+     int number1 = 9;
+    int number2 = 5;
+
+    if (number1 > number2) {
+        System.out.println("number1 is greater than number2");
     }
-}
+
+   //  else if(number1 < number2){
+   //    System.out.println("number1 is not greater than number2");
+   //  }
+
+   //  else{
+   //    System.out.println("number1 is equal to number2");
+   //  }
+
+
+        
+    //number1 is greater or equal to number2
+
+   //  if (number1 >= number2) {
+   //      System.out.println("number1 is greater or equal to number2");
+   //  }
+
+    //Changing variables value
+
+   //  int number1 = 15;
+   //  int number2 = 10;
+
+   //  if(number1 == number2){
+   //    System.out.println("number1 is equal to number2");
+   //  }
+
+   //  else if(number1 < number2){
+   //    System.out.println("number1 is smaller than number2");
+   //  }
+
+   //  else {
+   //    System.out.println("number1 is greater or equal to number2");
+   //  }
+
+
+       //if number1 and number2 is not equal
+
+      //  int number1 = 9;
+      //  int number2 = 9;
+
+         //  if (number1 != number2) {
+         //  System.out.println("number1 is not equal to number2");
+         //  }
+
+         //  else if(number1 > number2){
+         //    System.out.println("number1 is greater than number2");
+         //  }
+
+         //  else if(number1 < number2){
+         //    System.out.println("number1 is smaller than number2");
+         //  }
+            
+
+         //  else{
+         //    System.out.println("number1 is equal to number2 ");
+         //  }
+
+
+         //Test if number1 is NOT equal to number2
+
+         // if(number1 != number2){
+
+         //    System.out.println("number1 is not equal to number2");
+         // }
+
+         // else{
+         //    System.out.println("may be number1 is equal to number2");
+         // }
+   
+   // Three variables numbers
+
+   // int number1 = 10;
+   // int number2 = 4;
+   // int number3 = 5;
+
+   //if all numbers are equal
+
+   // if (number1 == number2 && number2 == number3){
+   //   System.out.println("All numbers are equal");
+   // }
+   // else if(number1 == number2 && number2 != number3);{
+   //    System.out.println("All numbers may not be equal");
+   // }
+
+   
+   //Test if number1 and number2 are equal OR number2 and number3 are equal
+
+   // if(number1 > number2 || number2 > number3){
+   //    System.out.println("number1 is greater than number2 or number2 is greater than number3");
+   // }
+
+   // else if(number1 < number2 || number3 < number2){
+   //    System.out.println("number1 is smaller than number2 or number3 is smaller than number2");
+   // }
+
+   // else{
+   //     System.out.println("number1 and number2 is equal or number2 and number3 is equal");
+   //  }
+
+
+
+
+   //Test if number1 is greater than number2 AND number1 is greater than number3
+
+      // if(number1 > number2 && number1 > number3){
+      //    System.out.println("number1 is greater than others");
+      // }
+
+      // else if(number1 == number2 && number1 == number3){
+      //    System.out.println("number1 is equal to others");
+      // }
+
+      // else if(number1 < number2 && number1 < number3){
+      //    System.out.println("number1 is smaller than others");
+      // }
+       
+
+      //number1 value changed from 4 to 10.
+      // else{ 
+
+      //    System.out.println("number1 is greater than number2 and number3 ");
+      // }
+
+
+
+      //Test if number1 is greater than number2. If not, test if number2 is greater than number3. (Google: C# else if )
+
+   //    int number1 = 3;
+   //    int number2 = 3;
+   //   int  number3 = 2;
+ 
+    // if number1 is greater than number2
+
+   //  if(number1 > number2){
+   //    System.out.println("number1 is greater than number2");
+   //  }
+
+
+
+   //Test if number1 and number2 are equal. If not, test if number1 and number3 are equal
+      
+         //if number1 and number2 are equal
+
+         // if(number1 == number2){
+         //    System.out.println("number1 and number2 are equal");
+         // }
+
+      //variable values changed
+
+      // int number1 = 5;
+      // int number2 = 8;
+      // int number3 = 5;
+
+      // if(number1 == number2){
+      //    System.out.println("number1 and number2 are equal");
+      // }
+
+      // else if(number1 == number3){
+      //    System.out.println("number1 and number3 are equal");
+      // }
+   
+
+
+      //Make three string typed variable – for example name1, name2 and name3. Assign value
+
+      
+
+    //    String name1 = "Hello";
+    //    String name2 = "Terve";
+    //    String name3 = "Moi";
+
+    //   //Test, if name1 and name2 are equal
+
+      // if(name1.equals(name2)){
+      //    System.out.println("name1 and name2 are equal");
+      // }
+     
+
+      // else{
+      //    System.out.println("name1 and name2 are not equal");
+      // }
+
+
+      //Test, if name1 and name2 are not equal
+
+      // if(name1.equals(name2)){
+      //    System.out.println("name1 and name2 are equal");
+      // }
+
+      // else if(!name1.equals(name2)){
+      //    System.out.println("name1 and name2 are not equal");
+      // }
+
+
+      
+      
+      //Test, if name1 and name2 are equal. If not, test if name1 and name3 are equal
+
+
+//     if(name1.equals(name2)){
+//       System.out.println("name1 and name2 are equal");
+//     }
+
+//     else if(name1.equals(name3)){
+//       System.out.println("name1 and name3 are equal");
+//     }
+
+//     else {
+//       System.out.println("name1 is not equal to name2 and name3");
+//     }
+      
+}}
+
